ff25c67c717184aa749f00aa6c0e011cd3ee07be
wming <btming217@gmail.com>
2019-03-16 20:57:04 +0800

    feat: test

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import logo from './logo.svg'
+import './App.css'
 
 class App extends Component {
   render() {
@@ -8,21 +8,19 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
+          <p>docker test </p>
           <a
             className="App-link"
             href="https://reactjs.org"
             target="_blank"
             rel="noopener noreferrer"
           >
-            Learn React
-          </a>
-        </header>
+            Learn React{' '}
+          </a>{' '}
+        </header>{' '}
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default App
